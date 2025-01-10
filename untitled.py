edeb4f542223a2b998f901069617508b78361187
Ju4n1 <Juan.feuilles@gmail.com>
2025-01-10 00:15:47 -0300

    Inicial
    lo básico if-for-while

---
+++ untitled.py
@@ -0,0 +1,24 @@
+print("Hola mundo")
+
+edad = 15
+
+
+if edad >= 18:
+   print ("Eres mayor de edad.")
+
+else:
+   print ("eres menor de edad.")
+
+frutas = ["manzana", "banana", "naranja"]
+
+
+for fruta in frutas:
+    print(fruta)
+
+contador = 0
+
+
+while contador < 5:
+
+    print(contador)
+    contador += 1
