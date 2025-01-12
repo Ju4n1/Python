5e98563ae16c9eb33b09ced5325cbb90c180bb40
Ju4n1 <Juan.feuilles@gmail.com>
2025-01-12 03:25:03 -0300

    entrada-salida
    recibir-retornar-datos

---
+++ entrada-salida.py
@@ -0,0 +1,18 @@
+#Esta función muestra un mensaje en la pantalla y espera a que el usuario ingrese
+# un valor.
+nombre = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+
+
+# print("Para mostrar información en la pantalla, utilizamos la función print().
+#  Esta función toma uno o más argumentos y los muestra en la consola.
+
+# Podemos utilizar la f-string (formateo de cadenas) para incrustar variables
+#  directamente dentro de una cadena de texto.Hola, " + nombre + "!")
+print("Tienes " + edad + " años.")
+
+nombre = "Juan"
+edad = 25
+
+
+print(f"Hola, mi nombre es {nombre} y tengo {edad} años.")
